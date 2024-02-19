1f9b0a7cf4475c2bc29e7e40ff16846947fb3afc
sadisi <sadisi.rajapaksha218346@gmail.com>
2024-02-20 01:29:36 +0530

    add event button and add area to enter event name

--- lib/Screens/calander_screen.dart
+++ lib/Screens/calander_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/etc/event.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalanderScreen extends StatefulWidget {
@@ -12,14 +13,19 @@ class _CalanderScreenState extends State<CalanderScreen> {
 //creating variables to focus day(current day)
   DateTime today = DateTime.now();
 
+  //store the events creayed
+Map <DateTime, List< Event>> events = {};
+
+//get user inputs to add a event
+TextEditingController _eventController = TextEditingController();
+  
+
 //capture the selected date
 void _onDaySelected(DateTime day, DateTime focusedDay) {
   setState(() {
     today = day;
   });
 }
-  
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +38,47 @@ void _onDaySelected(DateTime day, DateTime focusedDay) {
           'Calander',
         )
       ),
+    
+      //event add button
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          //show dilog for user to input event name
+          showDialog(
+            context: context,
+            builder: (context){
+              return AlertDialog(
+                scrollable: true,
+                title: Opacity(
+                      opacity: 0.7, 
+                      child: Text("Add an Event"),
+                     ),
+              
+                content: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: TextField(
+                    controller: _eventController,
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: (){},
+                     child: Text("Save"),
+                  ),
+                ]
+              );
+            }
+          );
+        },
+        child: Icon(Icons.add),
+        //add
+        backgroundColor: Color(0xFF674AEF),
+        foregroundColor: Colors.white,
+         shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(15.0), 
+         ),
+         //add end
+        ),
+
       //Calander 
       body: Padding(
         padding: const EdgeInsets.all(10.0),
