8dcce98c5ec6bca308f1a29f0b0c12047a0d9a6d
sadisi <sadisi.rajapaksha218346@gmail.com>
2024-02-20 00:07:02 +0530

    implemnt calander and set current date

--- lib/Screens/calander_screen.dart
+++ lib/Screens/calander_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalanderScreen extends StatefulWidget {
 
@@ -7,6 +8,11 @@ class CalanderScreen extends StatefulWidget {
 }
 
 class _CalanderScreenState extends State<CalanderScreen> {
+
+//creating variables to focus day(current day)
+  DateTime today = DateTime.now();
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +25,20 @@ class _CalanderScreenState extends State<CalanderScreen> {
           'Calander',
         )
       ),
+      //Calander 
+      body: Column(
+        children: [
+          TableCalendar(
+            locale: "en_US",
+            rowHeight: 43,
+            availableGestures: AvailableGestures.all,
+           focusedDay: today,
+           firstDay: DateTime.utc(2000,01,01),
+           lastDay: DateTime.utc(2050,12,31),
+           ),
+        ]
+      ),
+      //Calander end
      );
   }
 }
