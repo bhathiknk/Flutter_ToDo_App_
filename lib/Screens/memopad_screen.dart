a85fd98acc63bda1a01b11d4bfaf842f274300bf
bhathika <bhathikanileshkavindya@Gmail.com>
2024-03-29 09:34:17 +0530

    memopad connect with backend and implemented create,fetch,deleteoperations

--- lib/Screens/memopad_screen.dart
+++ lib/Screens/memopad_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_summernote/flutter_summernote.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MemoPadScreen());
@@ -13,7 +11,7 @@ class MemoPadScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter MemoPad',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -32,58 +30,139 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
-  GlobalKey<FlutterSummernoteState> _keyEditor = GlobalKey();
-  List<String> todoItems = [];
+  final TextEditingController _textEditingController = TextEditingController();
+  List<Map<String, dynamic>> memoItems = [];
+  late int userId;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserId();
+  }
+
+  Future<void> getUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    userId = prefs.getInt('userId') ?? -1;
+    fetchMemos(); // Fetch memos once userId is retrieved
+  }
+
+  Future<void> fetchMemos() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://10.0.2.2:8080/api/memos/$userId'),
+      );
+
+      if (response.statusCode == 200) {
+        List<dynamic> data = jsonDecode(response.body);
+        setState(() {
+          memoItems = List<Map<String, dynamic>>.from(data);
+        });
+      } else {
+        print('Failed to fetch memos: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      print('Error fetching memos: $e');
+    }
+  }
+
+  Future<void> _saveMemo() async {
+    final String memoText = _textEditingController.text.trim();
+
+    if (memoText.isNotEmpty) {
+      try {
+        final response = await http.post(
+          Uri.parse('http://10.0.2.2:8080/api/memos'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, dynamic>{
+            'userId': userId,
+            'content': memoText,
+          }),
+        );
+
+        if (response.statusCode == 201) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Success'),
+                content: Text('Memo text created successfully!'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+          setState(() {
+            memoItems.add({'content': memoText});
+            _textEditingController.clear();
+          });
+        } else {
+          print('Failed to save memo: ${response.reasonPhrase}');
+        }
+      } catch (e) {
+        print('Error saving memo: $e');
+      }
+    }
+  }
+
+  Future<void> _deleteMemo(int memoId) async {
+    try {
+      final response = await http.delete(
+        Uri.parse('http://10.0.2.2:8080/api/memos/$memoId'),
+      );
+
+      if (response.statusCode == 200) {
+        // Remove the memo from the memoItems list
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Success'),
+              content: Text('Memo text deleted successfully!'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+        setState(() {
+          memoItems.removeWhere((item) => item['id'] == memoId);
+        });
+      } else {
+        print('Failed to delete memo: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      print('Error deleting memo: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F3FF),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: Container(
-          width: double.infinity,
-          color: Color(0xFF674AEF),
-          child: AppBar(
-            backgroundColor: Colors.transparent,
-            title: Center(
-              child: Text(
-                widget.title,
-                style: TextStyle(
-                  fontSize: 22.0,
-                  color: Colors.black,
-                  fontFamily: 'Roboto-Regular',
-                ),
-              ),
-            ),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context); // Navigate back to the previous screen
-              },
-            ),
-            elevation: 0,
-            actions: <Widget>[
-              TextButton(
-                onPressed: () async {
-                  // Handle save button press
-                  final value = await _keyEditor.currentState?.getText();
-                  if (value != null && value.isNotEmpty) {
-                    // Call API to save memo
-                    await saveMemo(value);
-                  }
-                },
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 5),
-                  child: Text(
-                    'SAVE',
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                ),
-              ),
-            ],
+      appBar: AppBar(
+        backgroundColor: Color(0xFF674AEF),
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontSize: 22.0,
+            color: Colors.black,
+            fontFamily: 'Roboto-Regular',
           ),
         ),
+        centerTitle: true,
       ),
       body: Column(
         children: <Widget>[
@@ -92,39 +171,42 @@ class _NotesPageState extends State<NotesPage> {
               padding: EdgeInsets.only(
                 bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
-              child: FlutterSummernote(
-                hint: 'Your text here...',
-                key: _keyEditor,
-                customToolbar: """
-                  [
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['strikethrough', 'superscript', 'subscript']],
-                    ['insert', ['link', 'table', 'hr']]
-                  ]
-                """,
+              child: TextField(
+                controller: _textEditingController,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  hintText: "Enter your text here...",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
               ),
             ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 5,
+            color: Colors.white60,
           ),
           Expanded(
             child: ListView.builder(
               shrinkWrap: true,
-              itemCount: todoItems.length,
+              itemCount: memoItems.length,
               itemBuilder: (context, index) {
-                return buildContainer(todoItems[index]);
+                return buildContainer(memoItems[index]);
               },
             ),
           ),
+          buildSaveButton(),
         ],
       ),
     );
   }
 
-  Container buildContainer(String text) {
-    // Remove HTML tags
-    String cleanedText = text.replaceAll(RegExp(r'<[^>]*>'), '');
-
-    // Limit text to the first 12 words
-    String limitedText = cleanedText.split(' ').take(12).join(' ');
+  Widget buildContainer(Map<String, dynamic> item) {
+    final String cleanedText = item['content'].replaceAll(RegExp(r'<[^>]*>'), '');
+    final String limitedText = cleanedText.split(' ').take(12).join(' ');
 
     return Container(
       margin: EdgeInsets.all(12),
@@ -135,10 +217,7 @@ class _NotesPageState extends State<NotesPage> {
       height: 45,
       width: double.infinity,
       child: InkWell(
-        onTap: () {
-          // Populate Summernote text area with clicked text
-          _keyEditor.currentState?.setText(text);
-        },
+        onTap: () => setText(item['content']),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Row(
@@ -153,10 +232,7 @@ class _NotesPageState extends State<NotesPage> {
                   Icons.delete,
                   color: Colors.black,
                 ),
-                onPressed: () async {
-                  // Call API to delete memo
-                  await deleteMemo(text);
-                },
+                onPressed: () => _deleteMemo(item['id']),
               ),
             ],
           ),
@@ -165,49 +241,29 @@ class _NotesPageState extends State<NotesPage> {
     );
   }
 
-  Future<void> saveMemo(String text) async {
-    try {
-      // Make POST request to save memo
-      final response = await http.post(
-        Uri.parse('http://your-api-url/api/memos'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'content': text}),
-      );
-
-      if (response.statusCode == 201) {
-        // If memo saved successfully, add it to the list
-        setState(() {
-          todoItems.add(text);
-        });
-      } else {
-        // Handle error
-        print('Failed to save memo: ${response.statusCode}');
-      }
-    } catch (e) {
-      // Handle exceptions
-      print('Exception: $e');
-    }
+  void setText(String text) {
+    setState(() {
+      _textEditingController.text = text;
+    });
   }
 
-  Future<void> deleteMemo(String text) async {
-    try {
-      // Make DELETE request to delete memo
-      final response = await http.delete(
-        Uri.parse('http://your-api-url/api/memos/$text'),
-      );
-
-      if (response.statusCode == 200) {
-        // If memo deleted successfully, remove it from the list
-        setState(() {
-          todoItems.remove(text);
-        });
-      } else {
-        // Handle error
-        print('Failed to delete memo: ${response.statusCode}');
-      }
-    } catch (e) {
-      // Handle exceptions
-      print('Exception: $e');
-    }
+  Widget buildSaveButton() {
+    return GestureDetector(
+      onTap: _saveMemo,
+      child: Container(
+        width: double.infinity,
+        height: 60,
+        color: Colors.black87,
+        child: Center(
+          child: const Text(
+            'SAVE',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
